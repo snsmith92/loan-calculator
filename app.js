2cf54a6ead7c114350c75963d552ef84d312124e
snsmith92 <sherzelsmith@gmail.com>
2018-01-28 16:42:36 +0100

    Added loader

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-// Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+// Listen for submit, show loader for 2 seconds as soon as it is submitted, and delay calculate results
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide results 
+  document.getElementById('results').style.display = 'none';
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+  // Call calculateResults function after 1.5 seconds
+  setTimeout(calculateResults, 1500);
+
+  e.preventDefault();
+});
 
 
 // Calculate results function
-function calculateResults(e) {
+function calculateResults() {
   // UI variables
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -11,7 +20,6 @@ function calculateResults(e) {
   const monthlyPayment = document.getElementById('monthly-payment');
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
-
   const principal = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
@@ -24,30 +32,32 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    // Show results
+    document.getElementById('results').style.display = 'block';
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers!'); 
   }
-  e.preventDefault();
 }
 
 // Show Error function
 function showError(error){
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
   // Create a div
   const errorDiv = document.createElement('div');
-
   // Get elements (parent div)
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
-
   // Add bootstrap alert class
   errorDiv.className = "alert alert-danger";
-
   // Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error));
-
   // Insert error above heading
   card.insertBefore(errorDiv, heading);
-
   // Clear error after 3 seconds
   setTimeout(clearError, 3000);
 }
